a224c328fe65ecbcd75a635cee5648c2f1aae5b1
guimariz <guimariz@gmail.com>
2023-05-29 10:46:25 -0300

    logica de adicionar e remover blocos de cada tipo implementada

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
 from ursina import *
-
-class Test_cube(Entity):
-    def __init__(self):
-        super().__init__(
-            model = 'cube',
-            color = color.white,
-            texture = 'white_cube',
-            rotation = Vec3(45, 45, 45)
-        )
-
-def update():
-    if held_keys['a']:
-        test_square.x -= 4 * time.dt
+from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
-test_square = Entity(model = 'quad', color = color.red, scale = (1, 4), position = (5, 1))
+grass_texture = load_texture('assets/grass_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture = load_texture('assets/dirt_block.png')
 
-sans_texture = load_texture('assets/Sans.png')
-sans = Entity(model = 'quad', texture = sans_texture)
+block_pick = 1
 
-test_cube = Test_cube()
+def update():
+    global block_pick
+    
+    if held_keys['1']: block_pick = 1
+    if held_keys['2']: block_pick = 2
+    if held_keys['3']: block_pick = 3
+    if held_keys['4']: block_pick = 4
+
+
+class Voxel(Button):
+    def __init__(self, position = (0, 0, 0), texture = grass_texture):
+        super().__init__(
+            parent = scene,
+            position = position,
+            model = 'assets/block',
+            origin_y = 0.5,
+            texture = texture,
+            color = color.color(0, 0, random.uniform(0.9, 1)),
+            # highlight_color = color.lime,
+            scale = 0.5
+        )
+
+    def input(self, key):
+        if self.hovered:
+            if key == 'left mouse down':
+                if block_pick == 1: voxel = Voxel(position = self.position + mouse.normal, texture = grass_texture)
+                if block_pick == 2: voxel = Voxel(position = self.position + mouse.normal, texture = stone_texture)
+                if block_pick == 3: voxel = Voxel(position = self.position + mouse.normal, texture = brick_texture)
+                if block_pick == 4: voxel = Voxel(position = self.position + mouse.normal, texture = dirt_texture)
+                    
+            if key == 'right mouse down':
+                destroy(self)
+
+for z in range(20):
+    for x in range(20):
+        voxel = Voxel(position = (x, 0, z))
+
+player = FirstPersonController()
 
 app.run()
